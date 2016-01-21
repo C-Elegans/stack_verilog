bbde35c20aafeaec2584e54c11136acbd43879cc
C-Elegans <mtnolan2640@gmail.com>
2016-01-20 19:34:04 -0500

    finish adding jumps test

--- tests/jumps.v
+++ tests/jumps.v
@@ -5,9 +5,14 @@ initial $readmemh("jumps.hex",Mem);
 
 initial #1000 $finish();
 initial begin
-
+#56 `lassert(1);
+#98 `lassert(2);
+#98 `lassert(3);
+#98 `lassert(4);
+#98 `lassert(5);
+#98 `lassert(6);
+#60 `lassert(10);
+#28 `lassert(50);
 end
-
-
 
 endmodule 
