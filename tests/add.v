aaa1740ba54cbdbacdba993d48dd856b9628a9da
C-Elegans <mtnolan2640@gmail.com>
2016-01-24 20:08:50 -0500

    begin migrating tests to auto generated tests

--- tests/add.v
+++ tests/add.v
@@ -2,11 +2,10 @@
 module addtest();
 `include "../testinclude.vh"
 initial $readmemh("add.hex",Mem);
-
+initial $readmemh("add.val",values);
 initial #1000 $finish();
 initial begin
-values[0] = 4;
-values[1] =17;
+
 end
 
 
